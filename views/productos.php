b76d1d8a5a93a70c8597e158888837cc9398b59d
idali <159936431+IdaliLop@users.noreply.github.com>
2024-11-05 21:28:12 -0600

    Add files via upload

---
+++ views/productos.php
@@ -0,0 +1,63 @@
+
+<?php  
+    include_once __DIR__ . '/views/layouts/header.php';
+?>
+
+<section class="menu__contenedor">
+    <h2 class="texto__platillos">TODOS LOS PRODUCTOS</h2>    
+    <div class="botones-categorias">
+        <button class="alitas">Alitas</button>
+        <button class="chilaquiles">chilaquiles</button>
+        <button class="pizza">Pizza</button>
+        <button class="pollo">Pollo</button>
+        <button class="cenaduria">Cenaduria</button>
+        <button class="tacos">Tacos</button>
+        <button class="tapiocas">Tapiocas</button>
+    </div>
+    <div class="platillos__container">
+        <div class="platillo">
+            <img src="../snacks/img/hamburguesa.jpg" alt="producto1">
+            <h2>Nombre del Producto</h2>
+        </div>
+        <div class="precio">
+            <p>Precio del producto</p>
+            <button>Comprar</button>
+        </div>
+        <div class="platillo">
+            <img src="../Util/img/2.png" alt="producto2">
+            <h2>Nombre del Producto</h2>
+        </div>
+        <div class="precio">
+            <p>Precio del producto</p>
+            <button>Comprar</button>
+        </div>
+        <div class="platillo">
+            <img src="../Util/img/1.png" alt="producto3">
+            <h2>Nombre del Producto</h2>
+        </div>
+        <div class="precio">
+            <p>Precio del producto</p>
+            <button>Comprar</button>
+        </div>
+        <div class="platillo">
+            <img src="../Util/img/1.png" alt="producto5">
+            <h2>Nombre del Producto</h2>
+        </div>
+        <div class="precio">
+            <p>Precio del producto</p>
+            <button>Comprar</button>
+        </div>
+        <div class="platillo">
+            <img src="../Util/img/1.png" alt="producto5">
+            <h2>Nombre del Producto</h2>
+        </div>
+        <div class="platillo">
+            <img src="../Util/img/1.png" alt="producto5">
+            <h2>Nombre del Producto</h2>
+        </div>
+        <div class="precio">
+            <p>Precio del producto</p>
+            <button>Comprar</button>
+        </div>
+    </div>
+</section>
