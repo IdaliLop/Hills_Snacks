bfc08aac02baca6f1a04ebcf1e199fd8be0ebafe
idali <159936431+IdaliLop@users.noreply.github.com>
2024-11-24 19:57:02 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+verificar_sesion();
+function verificar_sesion(){
+    fetch('Controllers/UsuarioClienteController.php', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'funcion=verificar_sesion'
+  })
+  .then(response => {
+      console.log('Response:', response);  
+      return response.json();  
+  })
+  .then(data => {
+      console.log('Data:', data);
+      if (data.id) {
+          document.getElementById('login-link').style.display = 'none';
+          console.log("login");
+          console.log("Hay una sesion activa");
+      }
+      else{
+          console.log("No hay una sesion activa");
+      }
+  })
+  .catch(error => console.log('Error:', error));
+  }
